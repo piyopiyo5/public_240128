79e79ef434f6a0538970a25fae2e4edabee72347
piyopiyo5 <74588735+piyopiyo5@users.noreply.github.com>
2024-01-28 17:56:21 +0900

    機能追加。 　・ファイルのread&saveを追加。 　・ウィンドウ移動のマウスボタンをMiddleに変更。

--- Fusen/Fusen/Form1.cs
+++ Fusen/Fusen/Form1.cs
@@ -1,3 +1,6 @@
+using System;
+using System.IO;
+
 namespace Fusen
 {
     public partial class Form1 : Form
@@ -7,20 +10,28 @@ namespace Fusen
 
         public Form1()
         {
+            // 初期化
             InitializeComponent();
+            // 最初に保存ファイルを読み込む
+            if (File.Exists(@"fusen.txt"))
+            {
+                textFusenMemo.Text = File.ReadAllText(@"fusen.txt");
+            }
         }
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
-            if(e.KeyCode == Keys.Escape)
+            if (e.KeyCode == Keys.Escape)
             {
+                // テキストファイルに保存して終了
+                File.WriteAllText(@"fusen.txt", textFusenMemo.Text);
                 this.Close();
             }
         }
 
         private void Form1_MouseDown(object sender, MouseEventArgs e)
         {
-            if(e.Button == MouseButtons.Left)
+            if (e.Button == MouseButtons.Middle)
             {
                 this.mouseX = e.X;
                 this.mouseY = e.Y;
@@ -29,7 +40,7 @@ namespace Fusen
 
         private void Form1_MouseMove(object sender, MouseEventArgs e)
         {
-            if(e.Button == MouseButtons.Left)
+            if (e.Button == MouseButtons.Middle)
             {
                 this.Left += e.X - mouseX;
                 this.Top += e.Y - mouseY;
